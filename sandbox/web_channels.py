88ee5413842665e57ed7428eb5396edd15ead858
Martin Soto <donsoto@gmail.com>
2019-09-01 15:12:56 +0000

    Combine aiohttp with aiochan

--- sandbox/web_channels.py
+++ sandbox/web_channels.py
@@ -1,7 +1,9 @@
 import asyncio
+import itertools
 import logging
 import signal
 
+import aiochan as ac
 import aiohttp
 from aiohttp import web
 
@@ -12,8 +14,24 @@ logging.basicConfig(
 )
 
 
+async def salutate_the_world(in_chan):
+    salutations = [
+        "Hello, world!", "Hallo, Welt!", "Hola Mundo", "Salut Monde !",
+        "Ciao mondo!"
+    ]
+    for i in itertools.count():
+        out_chan: ac.Chan = await in_chan.get()
+        if out_chan is None:
+            break
+
+        await out_chan.put(salutations[i % len(salutations)])
+
+
 async def hello(request: web.Request) -> web.StreamResponse:
-    return web.Response(text="Hola mundo!")
+    salutate_chan: ac.Chan = request.app['salutate_chan']
+    salutation_chan = ac.Chan()
+    await salutate_chan.put(salutation_chan)
+    return web.Response(text=await salutation_chan.get())
 
 
 def create_app() -> web.Application:
@@ -30,16 +48,42 @@ async def main(host='0.0.0.0', port=8080):
         loop.add_signal_handler(
             s, lambda s=s: asyncio.create_task(shutdown(s, loop, main_task)))
 
-    runner = web.AppRunner(create_app())
+    salutate_chan = ac.Chan()
+    salutate_task = ac.go(salutate_the_world(salutate_chan))
+
+    app = create_app()
+    app['salutate_chan'] = salutate_chan
+
+    runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, host, port)
     await site.start()
     logging.info(f"Server running on {host}:{port}")
+
+    # Wait forever. Cancelling this task will finish the application.
     try:
         while True:
             await asyncio.sleep(3600)
+    except asyncio.CancelledError:
+        pass
     finally:
         await runner.cleanup()
+        salutate_chan.close()
+        await asyncio.gather(salutate_task)
+        await terminate_outstanding_tasks()
+
+
+async def terminate_outstanding_tasks():
+    outstanding_tasks = [
+        t for t in asyncio.all_tasks() if t is not asyncio.current_task()
+    ]
+    if not outstanding_tasks:
+        return
+
+    logging.info(f"Cancelling {len(outstanding_tasks)} outstanding task(s)")
+    for task in outstanding_tasks:
+        task.cancel()
+    await asyncio.gather(*outstanding_tasks, return_exceptions=True)
 
 
 async def shutdown(signal, loop: asyncio.BaseEventLoop,
@@ -48,26 +92,14 @@ async def shutdown(signal, loop: asyncio.BaseEventLoop,
 
     logging.info("Starting graceful shutdown")
     main_task.cancel()
-    await asyncio.gather(main_task, return_exceptions=True)
-
-    outstanding_tasks = [
-        t for t in asyncio.all_tasks() if t is not asyncio.current_task()
-    ]
-    if outstanding_tasks:
-        logging.info("Cancelling outstanding tasks")
-        for task in outstanding_tasks:
-            task.cancel()
-        await asyncio.gather(*outstanding_tasks, return_exceptions=True)
-
-    loop.stop()
 
 
 def start():
     loop = asyncio.get_event_loop()
+    loop.set_debug(True)
 
     try:
-        loop.create_task(main())
-        loop.run_forever()
+        loop.run_until_complete(main())
     finally:
         logging.info("Successfully shut down service")
         loop.close()
