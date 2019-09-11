f5c2e0e7d9e0cbe9335369b77936771e28372b83
Martin Soto <donsoto@gmail.com>
2019-09-11 21:00:45 +0000

    Conver Quart app to using trio.run

--- sandbox/quart_app.py
+++ sandbox/quart_app.py
@@ -1,11 +1,21 @@
-from quart_trio import QuartTrio
+import quart_trio
+import trio
+from hypercorn.config import Config
+from hypercorn.trio import serve
 
-app = QuartTrio(__name__)
+app = quart_trio.QuartTrio(__name__)
 
 
 @app.route('/')
 async def hello():
-    return 'hello'
+    return 'Hola!'
 
 
-app.run()
+def start():
+    config = Config()
+    config.bind = ["localhost:8080"]
+    trio.run(serve, app, config)
+
+
+if __name__ == '__main__':
+    start()
